48b4f3dd73538a6c83b42e8ac96f2cc61d25d460
Pratiti Chelleng <pratitichelleng@gmail.com>
2025-10-22 22:51:21 +0530

    Added Lab-1 C programing tasks
    hello.C
    introduction.C

---
+++ Lab-1/introduction.C
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main() {
+    printf("My name is Pratiti Chelleng.\n");
+    printf("I am persuing B.Tech in Mechanical Engineering from Tezpur University.\n");
+    printf("Roll No: MEB25033.");
+    return 0;
+}
